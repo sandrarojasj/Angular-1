849c9be89e79b1bbf93a405d0d0f61a07f6d02ba
sandraroj <nanarojas86@hotmail.com>
2022-07-15 13:45:36 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
+import { compileDeclareComponentFromMetadata } from '@angular/compiler';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FooterComponent } from './components/components/footer/footer.component';
+import { CompraComponent } from './components/compra/compra.component';
+import { HomeComponent } from './components/home/home.component';
+import { RegistroComponent } from './components/registro/registro.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path:'compra', component:CompraComponent},
+  {path: 'registro', component: RegistroComponent},
+  {path: '**', component: HomeComponent},
+  {path: 'footer', component:FooterComponent},
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { RegistroComponent } from './components/registro/registro.component';
+import { CompraComponent } from './components/compra/compra.component';
+import { FooterComponent } from './components/components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent,
+    HomeComponent,
+    RegistroComponent,
+    CompraComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
